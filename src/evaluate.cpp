37a8cb25bc261ac987bb367ad715beca2b1165a5
Praveen Tummala <praveentml@gmail.com>
2019-03-31 00:04:12 +0530

    take8

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -662,7 +662,7 @@ namespace {
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.
                 if (defendedSquares == squaresToQueen)
-                    k += 8;
+                    k += 7;
                 else if (defendedSquares & pos.pieces(Us, KNIGHT, BISHOP))
                     k += 8;
                 else if (defendedSquares & blockSq)
