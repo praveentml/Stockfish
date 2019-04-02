8779c76af6b539db53fe3362c4ebc2f7b84c94ce
Praveen Tummala <praveentml@gmail.com>
2019-04-02 22:39:46 +0530

    take1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -368,7 +368,7 @@ namespace {
 
             if (pos.slider_blockers(pos.pieces(Them, BISHOP), s, pinners))
 			{
-				score -= (((ThreatByMinor[ROOK] + (ThreatByRank * (int)relative_rank(Us, s))) * 2) / 3);
+				score -= (((ThreatByMinor[ROOK] + (ThreatByRank * (int)relative_rank(Us, s))) * 31) / 100);
 			}
         }
 
