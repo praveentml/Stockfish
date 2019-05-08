f8c478aa4e476b2148caf0ca8e87f74409ae4554
Praveen Tummala <praveentml@gmail.com>
2019-05-08 21:02:44 +0530

    tunevalue1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -153,7 +153,7 @@ namespace {
   constexpr Score TrappedRook        = S( 47,  4);
   constexpr Score WeakQueen          = S( 49, 15);
   constexpr Score WeakUnopposedPawn  = S( 12, 23);
-  constexpr Score KnightFlankAttacks = S( 17, 49);
+  constexpr Score KnightFlankAttacks = S( 11, 54);
 
 #undef S
 
