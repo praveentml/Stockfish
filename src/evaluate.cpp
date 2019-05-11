0cf72e11c12bb143c29b3c1a29b1f70d399b17d9
Praveen Tummala <praveentml@gmail.com>
2019-05-11 20:57:59 +0530

    take3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -475,7 +475,7 @@ namespace {
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
-    if (kingDanger > 90)
+    if (kingDanger > 80)
         score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
 
     // Penalty when our king is on a pawnless flank
