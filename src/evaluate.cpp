797139ec677804af516fcba71ffbcab8df2f2cd4
Praveen Tummala <praveentml@gmail.com>
2019-05-15 19:35:35 +0530

    take2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -307,8 +307,9 @@ namespace {
             // Bonus if piece is on an outpost square or can reach one
             bb = OutpostRanks & ~pe->pawn_attacks_span(Them);
             if (bb & s)
-                score += Outpost * (Pt == KNIGHT ? 6 : 3)
-                                 * ((attackedBy[Us][PAWN] & s) ? 3 : 1);
+                score += Outpost * (Pt == KNIGHT ? 4 : 2)
+                                 * ((attackedBy[Us][PAWN] & s) ? 2 : 1)
+								 * ((bb & kingRing[Them]) ? 2 : 1);
 
             else if (bb &= b & ~pos.pieces(Us))
                 score += Outpost * (Pt == KNIGHT ? 2 : 1)
