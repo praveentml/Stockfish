8468cb0607180af3cd37a17104f21df08f09bc2b
Praveen Tummala <praveentml@gmail.com>
2019-05-16 22:51:39 +0530

    tune1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -155,17 +155,17 @@ namespace {
   constexpr Score WeakUnopposedPawn  = S( 12, 23);
   int ii = 4*16, ij = 2*16, ik = 2*16, il = 1*16, im = 1*16, in = 1*16, ip= 2*16, iq = 1*16, ir = 2*16, is = 1*16, it = 1*16, iu = 1*16;
 	TUNE(SetRange(14, 114), ii);
-	TUNE(SetRange(-18, 82), ij);
-	TUNE(SetRange(-18, 82), ik);
-	TUNE(SetRange(-34, 66), il);
-	TUNE(SetRange(-34, 66), im);
-	TUNE(SetRange(-34, 66), in);
-	TUNE(SetRange(-18, 82), ip);
-	TUNE(SetRange(-34, 66), iq);
-	TUNE(SetRange(-18, 82), ir);
-	TUNE(SetRange(-34, 66), is);
-	TUNE(SetRange(-34, 66), it);
-	TUNE(SetRange(-34, 66), iu);
+	TUNE(SetRange(1, 82), ij);
+	TUNE(SetRange(1, 82), ik);
+	TUNE(SetRange(1, 66), il);
+	TUNE(SetRange(1, 66), im);
+	TUNE(SetRange(1, 66), in);
+	TUNE(SetRange(1, 82), ip);
+	TUNE(SetRange(1, 66), iq);
+	TUNE(SetRange(1, 82), ir);
+	TUNE(SetRange(1, 66), is);
+	TUNE(SetRange(1, 66), it);
+	TUNE(SetRange(1, 66), iu);
 #undef S
 
   // Evaluation class computes and stores attacks tables and other working data
@@ -319,14 +319,14 @@ namespace {
             // Bonus if piece is on an outpost square or can reach one
             bb = OutpostRanks & ~pe->pawn_attacks_span(Them);
             if (bb & s)
-                score += Outpost * (Pt == KNIGHT ? ii : ij)
+                score += Outpost * ((Pt == KNIGHT ? ii : ij)
                                  * ((attackedBy[Us][PAWN] & s) ? ik : il)
-								 * (b & attackedBy[Them][KING] ? im : in) / (16*16);
+								 * (b & attackedBy[Them][KING] ? im : in) / (16*16*16));
 
             else if (bb &= b & ~pos.pieces(Us))
-                score += Outpost * (Pt == KNIGHT ? ip : iq)
+                score += Outpost * ((Pt == KNIGHT ? ip : iq)
                                  * ((attackedBy[Us][PAWN] & bb) ? ir : is)
-								 * (b & attackedBy[Them][KING] ? it : iu) / (16*16);
+								 * (b & attackedBy[Them][KING] ? it : iu) / (16*16*16));
 
             // Knight and Bishop bonus for being right behind a pawn
             if (shift<Down>(pos.pieces(PAWN)) & s)
