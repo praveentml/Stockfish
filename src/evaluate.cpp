ae148ded6925673616f0bf907047f143fe1f44d7
Praveen Tummala <praveentml@gmail.com>
2019-05-19 00:56:52 +0530

    take1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -664,11 +664,13 @@ namespace {
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.
                 if (defendedSquares == squaresToQueen)
-                    k += 6;
-
+                    k += 8;
+                else if (passed_pawn_span(Us, s) & pos.pieces(Us, KNIGHT, BISHOP))
+                	k += 8;
                 else if (defendedSquares & blockSq)
-                    k += 4;
-
+                    k += 6;
+                else
+                    k += popcount(defendedSquares & attackedBy[Us][PAWN]);
                 bonus += make_score(k * w, k * w);
             }
         } // r > RANK_3
