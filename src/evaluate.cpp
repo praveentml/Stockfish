4c8589201dc365751f9658b26b4e73be02778fa0
Praveen Tummala <praveentml@gmail.com>
2019-05-20 09:36:11 +0530

    take1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -642,7 +642,7 @@ namespace {
                 bonus -= make_score(0, king_proximity(Us, blockSq + Up) * w);
 
             // If the pawn is free to advance, then increase the bonus
-            if (pos.empty(blockSq))
+            if (!(pos.pieces(Them) & blockSq & ~pos.pieces(Us, PAWN)))
             {
                 // If there is a rook or queen attacking/defending the pawn from behind,
                 // consider all the squaresToQueen. Otherwise consider only the squares
