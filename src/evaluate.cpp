a979bc0b3aa321ae17006a749d834663c22980d8
Praveen Tummala <praveentml@gmail.com>
2019-05-24 22:43:07 +0530

    take4

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -613,7 +613,7 @@ namespace {
       return std::min(distance(pos.square<KING>(c), s), 5);
     };
 
-    Bitboard b, bb, squaresToQueen, defendedSquares, unsafeSquares, daSquares, ddSquares;
+    Bitboard b, bb, squaresToQueen, defendedSquares, unsafeSquares, ddSquares;
     Score score = SCORE_ZERO;
 
     b = pe->passed_pawns(Us);
@@ -650,19 +650,18 @@ namespace {
                 defendedSquares = unsafeSquares = squaresToQueen = forward_file_bb(Us, s);
 
                 //double attacked but not double defended by enemy
-                //ddSquares = attackedBy2[Us] & ~attackedBy2[Them];
+                ddSquares = attackedBy2[Us] & ~attackedBy2[Them];
 
                 //double attacked but not double defended by us
-                daSquares = attackedBy2[Them] & ~attackedBy2[Us];
+                //daSquares = attackedBy2[Them] & ~attackedBy2[Us];
 
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN);
 
                 if (!(pos.pieces(Us) & bb))
-                    defendedSquares &= (attackedBy[Us][ALL_PIECES] & ~daSquares);
+                    defendedSquares &= (attackedBy[Us][ALL_PIECES]);
 
                 if (!(pos.pieces(Them) & bb))
-                	unsafeSquares &= (attackedBy[Them][ALL_PIECES] | pos.pieces(Them));
-                    //unsafeSquares &= ((attackedBy[Them][ALL_PIECES] | pos.pieces(Them)) & ~ddSquares);
+                    unsafeSquares &= ((attackedBy[Them][ALL_PIECES] | pos.pieces(Them)) & ~ddSquares);
 
                 // If there aren't any enemy attacks, assign a big bonus. Otherwise
                 // assign a smaller bonus if the block square isn't attacked.
