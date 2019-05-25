5a63f43d75fe6ff1c7ac8b43fb85151aefef2f01
Praveen Tummala <praveentml@gmail.com>
2019-05-25 13:07:49 +0530

    take3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -630,7 +630,7 @@ namespace {
 
         if (r > RANK_3)
         {
-            int w = (r-2) * (r-2) + 2;
+            int w = (r-1) * (r-1) + 2;
             Square blockSq = s + Up;
 
             // Adjust bonus based on the king's proximity
