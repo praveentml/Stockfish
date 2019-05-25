d19f0ba5400c8bad2b43fb14d64051d48c8cbc12
Praveen Tummala <praveentml@gmail.com>
2019-05-25 10:26:07 +0530

    take1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -652,7 +652,7 @@ namespace {
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN);
 
                 if (!(pos.pieces(Us) & bb))
-                    defendedSquares &= attackedBy[Us][ALL_PIECES];
+                    defendedSquares &= attackedBy[Us][ALL_PIECES] | (attacks_bb<ROOK>(s, pos.pieces(Them, PAWN)) & pos.pieces(Us, ROOK, QUEEN)) ;
 
                 if (!(pos.pieces(Them) & bb))
                     unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
