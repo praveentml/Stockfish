dc8c7c88bd0be969b0d212f9df251a59e47c3688
Praveen Tummala <praveentml@gmail.com>
2019-05-26 11:04:00 +0530

    take3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -81,10 +81,10 @@ namespace {
   constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 77, 55, 44, 10 };
 
   // Penalties for enemy's safe checks
-  constexpr int QueenSafeCheck  = 790;
-  constexpr int RookSafeCheck   = 1090;
-  constexpr int BishopSafeCheck = 645;
-  constexpr int KnightSafeCheck = 800;
+  constexpr int QueenSafeCheck  = 770;
+  constexpr int RookSafeCheck   = 1070;
+  constexpr int BishopSafeCheck = 625;
+  constexpr int KnightSafeCheck = 780;
 
 #define S(mg, eg) make_score(mg, eg)
 
