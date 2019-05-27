456999f870078932818cba4533b97d8ace8a9a19
Praveen Tummala <praveentml@gmail.com>
2019-05-27 14:17:57 +0530

    take1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -659,7 +659,7 @@ namespace {
 
                 // If there aren't any enemy attacks, assign a big bonus. Otherwise
                 // assign a smaller bonus if the block square isn't attacked.
-                int k = !unsafeSquares ? 20 : !(unsafeSquares & blockSq) ? 9 : 0;
+                int k = !unsafeSquares ? 20 : (blockSq & (attackedBy[Us][KNIGHT] | attackedBy[Us][BISHOP] | attackedBy[Us][PAWN])) ? 12 : !(unsafeSquares & blockSq) ? 9 : 0;
 
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.
