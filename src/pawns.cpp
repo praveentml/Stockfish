8372053b68a85d3213aea3f452e1b66db307ffd4
Praveen Tummala <praveentml@gmail.com>
2019-05-28 16:16:26 +0530

    take3

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -203,7 +203,7 @@ void Entry::evaluate_shelter(const Position& pos, Square ksq, Score& shelter) {
       {
     	  Bitboard b1 = pawn_attacks_bb<Them>(theirPawns) & KingFlank[file_of(ksq)] & ~ourPawns;
     	  if((relative_rank(Us, ksq) == RANK_2 && theirRank == RANK_4) && b1)
-    		  bonus[MG] -= 123 , bonus[EG] -= 123;
+    		  bonus[MG] -= ((UnblockedStorm[d][theirRank] * 80) / 100);
     	  else
     		  bonus[MG] -= 82 * (theirRank == RANK_3), bonus[EG] -= 82 * (theirRank == RANK_3);
       }
