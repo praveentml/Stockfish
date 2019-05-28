2e055b54d0744e115797e6e2c8e7d52da8d67e8e
Praveen Tummala <praveentml@gmail.com>
2019-05-28 19:52:08 +0530

    take1

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -200,7 +200,7 @@ void Entry::evaluate_shelter(const Position& pos, Square ksq, Score& shelter) {
       bonus[MG] += ShelterStrength[d][ourRank];
 
       if (ourRank && (ourRank == theirRank - 1))
-          bonus[MG] -= 112 * (theirRank == RANK_3), bonus[EG] -= 112 * (theirRank == RANK_3);
+          bonus[MG] -= (UnblockedStorm[d][theirRank] / 2) * (theirRank == RANK_3), bonus[EG] -= (UnblockedStorm[d][theirRank] / 2) * (theirRank == RANK_3);
       else
           bonus[MG] -= UnblockedStorm[d][theirRank];
   }
