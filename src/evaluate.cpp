a214ad4b345037c507df5e7080ca00cef9e075c5
Praveen Tummala <praveentml@gmail.com>
2019-06-04 14:39:28 +0530

    take1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -558,7 +558,7 @@ namespace {
     score += RestrictedPiece * popcount(b);
 
     score += make_score(0, 6) * popcount(attackedBy[Them][QUEEN]
-			& (attackedBy[Us][ALL_PIECES] ^ attackedBy[Us][KING] ^ attackedBy[Us][QUEEN]));
+			& (attackedBy[Us][ALL_PIECES] & ~(attackedBy[Us][KING] | attackedBy[Us][QUEEN])));
 
     // Bonus for enemy unopposed weak pawns
     if (pos.pieces(Us, ROOK, QUEEN))
