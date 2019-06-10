99f1efcd98d09b2b2922cd806e792a65e9bfe673
Praveen Tummala <praveentml@gmail.com>
2019-06-10 22:55:16 +0530

    take1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -698,7 +698,7 @@ namespace {
     // Find the available squares for our pieces inside the area defined by SpaceMask
     Bitboard safe =   SpaceMask
                    & ~pos.pieces(Us, PAWN)
-                   & ~attackedBy[Them][PAWN];
+                   & ~attackedBy[Them][ALL_PIECES];
 
     // Find all squares which are at most three squares behind some friendly pawn
     Bitboard behind = pos.pieces(Us, PAWN);
