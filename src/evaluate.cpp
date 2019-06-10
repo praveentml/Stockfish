35102a0d63e642cc01a855fd5e187eb8f55e9def
Praveen Tummala <praveentml@gmail.com>
2019-06-10 23:57:18 +0530

    take2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -695,10 +695,13 @@ namespace {
       Us == WHITE ? CenterFiles & (Rank2BB | Rank3BB | Rank4BB)
                   : CenterFiles & (Rank7BB | Rank6BB | Rank5BB);
 
+    Bitboard stronglyProtected =  attackedBy[Them][PAWN]
+                       | (attackedBy2[Them] & ~attackedBy2[Us]);
+
     // Find the available squares for our pieces inside the area defined by SpaceMask
     Bitboard safe =   SpaceMask
                    & ~pos.pieces(Us, PAWN)
-                   & ~attackedBy[Them][ALL_PIECES];
+                   & ~stronglyProtected;
 
     // Find all squares which are at most three squares behind some friendly pawn
     Bitboard behind = pos.pieces(Us, PAWN);
