4c563b06b51c1864033bf547370fb4f0ea55d62a
praveentml <47745562+praveentml@users.noreply.github.com>
2019-10-08 14:38:51 +0530

    tk2

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,7 +132,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (3 + bool(phalanx) - (1 + bool(opposed)))
+            int v =  Connected[r] * (3 + bool(phalanx) - (1 - bool(!opposed)))
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
