5a9923d0469bdcd79e3f95c1bdbdb73a4af7cbd9
home <praveentml@gmail.com>
2019-10-21 18:53:40 +0530

    take1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -342,7 +342,7 @@ namespace {
         if (Pt == ROOK)
         {
             // Bonus for rook on the same file as a queen
-            if (file_bb(s) & pos.pieces(QUEEN))
+            if (file_bb(s) & pos.pieces(QUEEN, ROOK))
                 score += RookOnQueenFile;
 
             // Bonus for rook on an open or semi-open file
