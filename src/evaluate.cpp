0bf201da3f4511d23b57c0cf2aced83e7b4f8bce
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2019-12-22 22:01:13 +0530

    tak2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -337,7 +337,7 @@ namespace {
         if (Pt == ROOK)
         {
             // Bonus for rook on the same file as a queen
-            if (file_bb(s) & (pos.pieces(QUEEN) | more_than_one(pos.pieces(ROOK))))
+            if ((file_bb(s) & pos.pieces(QUEEN)) || (more_than_one(file_bb(s) & pos.pieces(ROOK))))
                 score += RookOnQueenFile;
 
             // Bonus for rook on an open or semi-open file
