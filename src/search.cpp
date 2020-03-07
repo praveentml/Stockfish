c1f8493c797cdc1e56b3357fb10debb8c78831c5
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2020-03-07 14:25:36 +0530

    tk1

--- src/search.cpp
+++ src/search.cpp
@@ -1156,7 +1156,7 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
-                  r++;
+            	  r += 2;
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
