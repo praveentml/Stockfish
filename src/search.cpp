0c12729444314bafb5351e4361ca91cda03cc1b3
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2020-03-31 11:30:37 +0530

    tk1

--- src/search.cpp
+++ src/search.cpp
@@ -1163,7 +1163,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 2;
+                  r += 2 - formerPv;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
