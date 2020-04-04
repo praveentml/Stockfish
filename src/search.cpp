9549824dc9c0663d8e92549f16bad02bb628f210
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2020-04-04 11:38:08 +0530

    tk1

--- src/search.cpp
+++ src/search.cpp
@@ -1227,7 +1227,7 @@ moves_loop: // When in check, search starts from here
                                         : -stat_bonus(newDepth);
 
               if (move == ss->killers[0])
-                  bonus += bonus / 4;
+                  bonus += bonus / 3;
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
