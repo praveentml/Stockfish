7627043db196e41fc586a46e685594b03c5ec2e0
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2020-04-05 22:11:35 +0530

    tk1

--- src/position.h
+++ src/position.h
@@ -96,6 +96,7 @@ public:
   template<PieceType Pt> const Square* squares(Color c) const;
   template<PieceType Pt> Square square(Color c) const;
   bool is_on_semiopen_file(Color c, Square s) const;
+  bool is_on_semiopen_rank(Color c, Square s) const;
 
   // Castling
   int castling_rights(Color c) const;
@@ -262,6 +263,10 @@ inline Square Position::ep_square() const {
 
 inline bool Position::is_on_semiopen_file(Color c, Square s) const {
   return !(pieces(c, PAWN) & file_bb(s));
+}
+
+inline bool Position::is_on_semiopen_rank(Color c, Square s) const {
+  return !(pieces(c, PAWN) & rank_bb(s));
 }
 
 inline bool Position::can_castle(CastlingRights cr) const {
