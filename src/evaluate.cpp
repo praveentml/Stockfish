97e47fe0a05b6d55dc8354a1c84812e029b40c6d
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2020-04-06 12:38:41 +0530

    tk1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -448,6 +448,7 @@ namespace {
                  + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
+    			 +  80 * popcount(attackedBy[Them][PAWN] & KingFlank[file_of(ksq)] & Camp)
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
