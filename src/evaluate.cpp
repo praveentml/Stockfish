6ece5b781979443825b458ab83d1caf9e345bf74
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2020-04-06 12:33:33 +0530

    tk2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -337,7 +337,7 @@ namespace {
         if (Pt == ROOK)
         {
             // Bonus for rook on the same file as a queen
-            if ((file_bb(s) | rank_bb(s)) & pos.pieces(QUEEN))
+            if (file_bb(s) & pos.pieces(QUEEN))
                 score += RookOnQueenFile;
 
             // Bonus for rook on an open or semi-open file
@@ -448,6 +448,7 @@ namespace {
                  + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
+    			 +  80 * attackedBy[Them][PAWN] & KingFlank[file_of(ksq)] & Camp
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
