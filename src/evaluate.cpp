ccc864a546834cfbf95e238028c294059530a79a
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2020-04-13 15:44:33 +0530

    tk

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -446,7 +446,7 @@ namespace {
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  + 185 * popcount(kingRing[Us] & weak)
-                 + 148 * popcount(unsafeChecks)
+                 + 148 * popcount(unsafeChecks) * (1 + !pos.castling_rights(Us))
                  +  98 * popcount(pos.blockers_for_king(Us))
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
