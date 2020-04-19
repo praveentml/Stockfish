d79f18bc6e129d6383b4ec096636744aa4b8cb2b
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2020-04-19 14:40:56 +0530

    take1

--- src/search.cpp
+++ src/search.cpp
@@ -1114,7 +1114,7 @@ moves_loop: // When in check, search starts from here
       else if (   move == ss->killers[0]
                && pos.advanced_pawn_push(move)
                && pos.pawn_passed(us, to_sq(move)))
-          extension = 1;
+          extension = 1 + formerPv;
 
       // Last captures extension
       else if (   PieceValue[EG][pos.captured_piece()] > PawnValueEg
