3f67e2f865d18eb4c63b0eaba43917b84f2a63ec
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2020-04-30 21:08:31 +0530

    tk1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -135,6 +135,7 @@ namespace {
   constexpr Score KnightOnQueen       = S( 16, 11);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
   constexpr Score MinorBehindPawn     = S( 18,  3);
+  constexpr Score RookPawnBlockade    = S( 16,  5);
   constexpr Score Outpost             = S( 30, 21);
   constexpr Score PassedFile          = S( 11,  8);
   constexpr Score PawnlessFlank       = S( 17, 95);
@@ -652,6 +653,11 @@ namespace {
                     k += 5;
 
                 bonus += make_score(k * w, k * w);
+            }
+            else
+            {
+            	if (pos.pieces(Them, ROOK) & ~(pe->pawn_attacks_span(Us) | (attackedBy[Us  ][KNIGHT] | attackedBy[Us  ][BISHOP])) & blockSq)
+            	    score -= RookPawnBlockade;
             }
         } // r > RANK_3
 
