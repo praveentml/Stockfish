d7f0fb6dae03cd33ce409bd27bc6262e233132d4
Praveen tummala <praveentummala@Praveens-MacBook-Pro.local>
2020-04-30 22:06:12 +0530

    tk1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -131,7 +131,7 @@ namespace {
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);
   constexpr Score Hanging             = S( 69, 36);
-  constexpr Score KingProtector       = S(  7,  8);
+  constexpr Score KingProtector       = S(  8, 11);
   constexpr Score KnightOnQueen       = S( 16, 11);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
   constexpr Score MinorBehindPawn     = S( 18,  3);
