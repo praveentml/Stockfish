2ca44e7d6999ca0c043778dc47fc380f2d09d31c
praveentml <praveentml@gmail.com>
2023-05-30 00:23:33 +0530

    sear2

--- src/search.cpp
+++ src/search.cpp
@@ -1113,6 +1113,9 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5480)
               extension = 1;
+
+          else if(to_sq(countermove) == prevSq)
+        	  extension = 1;
       }
 
       // Add extension to new depth
