00ff57a1dc8650545d8a788d2374db2e95e3fb11
praveentml <praveentml@gmail.com>
2024-01-19 22:17:44 +0530

    try1

--- src/search.cpp
+++ src/search.cpp
@@ -1081,7 +1081,7 @@ moves_loop:  // When in check, search starts here
                     singularQuietLMR = !ttCapture;
 
                     // Avoid search explosion by limiting the number of double extensions
-                    if (!PvNode && value < singularBeta - 17 && ss->doubleExtensions <= 11)
+                    if (!PvNode && value < singularBeta - 17 && ss->doubleExtensions <= (depth < 10 ? 7 : depth < 15 ? 9 : 11))
                     {
                         extension = 2;
                         depth += depth < 15;
