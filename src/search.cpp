fa22555124f29be119621c5fa1c99623d6d92b16
praveentml <praveentml@gmail.com>
2024-01-23 21:57:16 +0530

    try1

--- src/search.cpp
+++ src/search.cpp
@@ -1239,6 +1239,8 @@ moves_loop:  // When in check, search starts here
             if (!ttMove)
                 r += 2;
 
+            if(capture)
+				r++;
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3), !cutNode);
         }
